3641ce6dd02c1269cf1a2d4276ee654c29e5fcaf
Uptivity <uptivityappsuk@gmail.com>
2025-06-05 17:11:46 +0100

    Add files via upload

--- resources/views/profile/edit.blade.php
+++ resources/views/profile/edit.blade.php
@@ -12,6 +12,7 @@
 
         <form method="POST" action="{{ route('profile.update') }}">
             @csrf
+            @method('PATCH')
 
             <div class="grid grid-cols-1 md:grid-cols-2 gap-4">
                 <div>
